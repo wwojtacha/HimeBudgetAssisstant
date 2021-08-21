b78d290beb5bf3267ea42aed5ef505d9dae7a29a
wwojtacha <wwojtacha@o2.pl>
2021-08-21 20:31:50 +0200

    spring project init commit

---
+++ assisstant/src/main/java/home/budget/assisstant/AssisstantApplication.java
@@ -0,0 +1,13 @@
+package home.budget.assisstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssisstantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssisstantApplication.class, args);
+	}
+
+}
---
+++ assisstant/src/test/java/home/budget/assisstant/AssisstantApplicationTests.java
@@ -0,0 +1,13 @@
+package home.budget.assisstant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssisstantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
