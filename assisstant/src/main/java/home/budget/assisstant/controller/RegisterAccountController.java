e62e0f83a17796742e9d5989646ee54372ef1516
wwojtacha <wwojtacha@o2.pl>
2021-08-22 20:01:58 +0200

    add endpoint to recharge register account balance

--- assisstant/src/main/java/home/budget/assisstant/controller/RegisterAccountController.java
+++ assisstant/src/main/java/home/budget/assisstant/controller/RegisterAccountController.java
@@ -1,10 +1,12 @@
 package home.budget.assisstant.controller;
 
 import home.budget.assisstant.logic.RegisterAccountService;
+import home.budget.assisstant.model.RechargeAmount;
 import home.budget.assisstant.model.RegisterAccount;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,13 @@ public class RegisterAccountController {
     @ResponseStatus(HttpStatus.OK)
     public List<RegisterAccount> getAllRegisterAccounts() {
         return registerAccountService.getRegisterAccounts();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RegisterAccount rechargeRegisterAccount(
+            @PathVariable Long id,
+            @RequestBody @Valid RechargeAmount rechargeAmount) {
+        return registerAccountService.rechargeRegisterAccount(id, rechargeAmount);
     }
 }
