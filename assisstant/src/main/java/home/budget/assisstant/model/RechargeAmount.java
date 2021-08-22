9e1e82945cc45e66a4dea3b0f50e2ce320055391
wwojtacha <wwojtacha@o2.pl>
2021-08-22 19:57:17 +0200

    add logic to recharge existing register account

---
+++ assisstant/src/main/java/home/budget/assisstant/model/RechargeAmount.java
@@ -0,0 +1,12 @@
+package home.budget.assisstant.model;
+
+import java.math.BigDecimal;
+
+public class RechargeAmount {
+
+    final private BigDecimal rechargeValue = BigDecimal.ZERO;
+
+    public BigDecimal getRechargeValue() {
+        return rechargeValue;
+    }
+}
