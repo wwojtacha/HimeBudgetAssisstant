d49b7155f2de6e7ed332b78bee259e0904535d7f
wwojtacha <wwojtacha@o2.pl>
2021-08-22 19:59:21 +0200

    add logic updating register account balance

--- assisstant/src/main/java/home/budget/assisstant/logic/RegisterAccountService.java
+++ assisstant/src/main/java/home/budget/assisstant/logic/RegisterAccountService.java
@@ -1,11 +1,16 @@
 package home.budget.assisstant.logic;
 
+import home.budget.assisstant.model.RechargeAmount;
 import home.budget.assisstant.model.RegisterAccount;
 import home.budget.assisstant.repository.RegisterAccountRepository;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RegisterAccountService {
@@ -18,5 +23,25 @@ public class RegisterAccountService {
 
     public List<RegisterAccount> getRegisterAccounts() {
         return registerAccountRepository.findAll(Sort.by(Sort.Order.desc("name")));
+    }
+
+    public RegisterAccount rechargeRegisterAccount(final Long id, final RechargeAmount rechargeAmount) {
+
+        final Optional<RegisterAccount> dbRegisterAccount = registerAccountRepository.findById(id);
+
+        if (dbRegisterAccount.isEmpty()) {
+            final String message = String.format("Register account with id: %s does not exist.", id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+        }
+
+        final RegisterAccount registerAccount = dbRegisterAccount.get();
+        final BigDecimal previousBalance = registerAccount.getBalance();
+
+        BigDecimal rechargeValue = rechargeAmount.getRechargeValue();
+
+        final BigDecimal currentBalance = previousBalance.add(rechargeValue);
+        registerAccount.setBalance(currentBalance);
+
+        return registerAccountRepository.save(registerAccount);
     }
 }
