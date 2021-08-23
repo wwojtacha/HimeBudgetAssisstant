c55bd4ffda82e783bb1b26fb5e1f63749157432e
wwojtacha <wwojtacha@o2.pl>
2021-08-23 10:07:42 +0200

    add tests to check money transfer between two register accounts

--- assisstant/src/test/java/home/budget/assisstant/AssistantApplicationTests.java
+++ assisstant/src/test/java/home/budget/assisstant/AssistantApplicationTests.java
@@ -1,6 +1,7 @@
 package home.budget.assisstant;
 
 import home.budget.assisstant.logic.RegisterAccountService;
+import home.budget.assisstant.model.RegisterAccount;
 import home.budget.assisstant.model.TransferAmount;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -57,6 +59,93 @@ class AssistantApplicationTests {
 		final boolean isMessage = message != null && !message.isEmpty();
 
 		assertTrue(isMessage);
+	}
+
+	@Test
+	void checkIfNegativeValueTransferCanBeMade() {
+		final TransferAmount transferAmount = new TransferAmount(BigDecimal.valueOf(-100));
+
+		final Exception exception = assertThrows(ResponseStatusException.class, () -> {
+			registerAccountService.transferMoney(1L, 4L, transferAmount);
+		});
+
+		final String message = exception.getMessage();
+		final boolean isMessage = message != null && !message.isEmpty();
+
+		final String failedTestMessage =
+				"Expected to get an exception not allowing for a negative value transfer";
+
+		assertTrue(isMessage, failedTestMessage);
+	}
+
+	@Test
+	void checkIfTransferFromNonExistingRegisterAccountCanBeMade() {
+		final TransferAmount transferAmount = new TransferAmount(BigDecimal.valueOf(100));
+
+		final Exception exception = assertThrows(ResponseStatusException.class, () -> {
+			registerAccountService.transferMoney(777777L, 4L, transferAmount);
+		});
+
+		final String message = exception.getMessage();
+		final boolean isMessage = message != null && !message.isEmpty();
+
+		final String failedTestMessage =
+				"Expected to get an exception not allowing for transfer from non-existing register account";
+
+		assertTrue(isMessage, failedTestMessage);
+	}
+
+	@Test
+	void checkIfTransferToNonExistingRegisterAccountCanBeMade() {
+		final TransferAmount transferAmount = new TransferAmount(BigDecimal.valueOf(100));
+
+		final Exception exception = assertThrows(ResponseStatusException.class, () -> {
+			registerAccountService.transferMoney(1L, 888888L, transferAmount);
+		});
+
+		final String message = exception.getMessage();
+		final boolean isMessage = message != null && !message.isEmpty();
+
+		final String failedTestMessage =
+				"Expected to get an exception not allowing for transfer to non-existing register account";
+
+		assertTrue(isMessage, failedTestMessage);
+	}
+
+	@Test
+	void checkIfValidTransferCanBeMade() {
+		final TransferAmount transferAmount = new TransferAmount(BigDecimal.valueOf(200));
+
+		registerAccountService.transferMoney(1L, 4L, transferAmount);
+
+		final List<RegisterAccount> registerAccounts = registerAccountService.getRegisterAccounts();
+
+		final RegisterAccount walletRegisterAccount = registerAccounts
+				.stream()
+				.filter(x -> "Wallet".equals(x.getName())).findFirst()
+				.orElseGet(RegisterAccount::new);
+
+		final RegisterAccount foodRegisterAccount = registerAccounts
+				.stream()
+				.filter(x -> "Food expenses".equals(x.getName())).findFirst()
+				.orElseGet(RegisterAccount::new);
+
+		final String walletName = walletRegisterAccount.getName();
+		final BigDecimal walletBalance = walletRegisterAccount.getBalance();
+
+		final String foodName = foodRegisterAccount.getName();
+		final BigDecimal foodBalance = foodRegisterAccount.getBalance();
+
+		final boolean isWalletBalanceDecreased = walletName != null && walletBalance.compareTo(BigDecimal.valueOf(800)) == 0;
+		final boolean isFoodBalanceIncreased = foodName != null && foodBalance.compareTo(BigDecimal.valueOf(200)) == 0;
+
+		final String message = "Wallet and/or food balances should have other values";
+
+		assertTrue(isWalletBalanceDecreased && isFoodBalanceIncreased, message);
+
+//		restore balances to initial values so as not to change DB entries, because the same test next time would fail
+		registerAccountService.transferMoney(4L, 1L, transferAmount);
+
 	}
 
 }
